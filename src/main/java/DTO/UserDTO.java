0b76d4b1e612da8a3a81324ea484c82f5c33c6b5
ponion <114641838+onionkungya@users.noreply.github.com>
2024-04-11 13:05:48 +0900

    Update UserDTO.java

--- src/main/java/DTO/UserDTO.java
+++ src/main/java/DTO/UserDTO.java
@@ -1,4 +1,62 @@
 package DTO;
 
 public class UserDTO {
+  private String UserID;
+	private String UserName;
+	private String Email;
+	private String PhoneNumber;
+	private String UserSNN;
+	
+	public UserDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserDTO(String userID, String userName, String email, String phoneNumber, String userSNN) {
+		this.UserID = userID;
+		this.UserName = userName;
+		this.Email = email;
+		this.PhoneNumber = phoneNumber;
+		this.UserSNN = userSNN;
+	}
+
+	public String getUserID() {
+		return UserID;
+	}
+
+	public void setUserID(String userID) {
+		this.UserID = userID;
+	}
+
+	public String getUserName() {
+		return UserName;
+	}
+
+	public void setUserName(String userName) {
+		this.UserName = userName;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		this.Email = email;
+	}
+
+	public String getPhoneNumber() {
+		return PhoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.PhoneNumber = phoneNumber;
+	}
+
+	public String getUserSNN() {
+		return UserSNN;
+	}
+
+	public void setUserSNN(String userSNN) {
+		this.UserSNN = userSNN;
+	}
+	
 }
