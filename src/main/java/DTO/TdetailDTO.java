18b27fe30aad4e71f0d1fa34558522efc38da90f
jaeyoonjung(rexyoon) <137620925+rexyoon@users.noreply.github.com>
2024-04-11 13:11:25 +0900

    Update TdetailDTO.java

--- src/main/java/DTO/TdetailDTO.java
+++ src/main/java/DTO/TdetailDTO.java
@@ -1,4 +1,73 @@
 package DTO;
 
 public class TdetailDTO {
+	private String TDetailID;
+	private String TransactionID;
+	private String ProductID;
+	private int Purchase;
+	private int UnitPrice;
+	public TdetailDTO() {}
+	
+	public TdetailDTO(String tDetailID, String transactionID, String productID, int purchase, int unitPrice) {
+		this.TDetailID = tDetailID;
+		this.TransactionID = transactionID;
+		this.ProductID = productID;
+		this.Purchase = purchase;
+		this.UnitPrice = unitPrice;
+	}
+
+
+
+
+
+	public String getTDetailID() {
+		return TDetailID;
+	}
+
+	public void setTDetailID(String tDetailID) {
+		TDetailID = tDetailID;
+	}
+
+	public String getTransactionID() {
+		return TransactionID;
+	}
+
+	public void setTransactionID(String transactionID) {
+		TransactionID = transactionID;
+	}
+
+	public String getProductID() {
+		return ProductID;
+	}
+
+	public void setProductID(String productID) {
+		ProductID = productID;
+	}
+
+	public int getPurchase() {
+		return Purchase;
+	}
+
+	public void setPurchase(int purchase) {
+		Purchase = purchase;
+	}
+
+	public int getUnitPrice() {
+		return UnitPrice;
+	}
+
+	public void setUnitPrice(int unitPrice) {
+		UnitPrice = unitPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "TdetailDTO [TDetailID=" + TDetailID + ", TransactionID=" + TransactionID + ", ProductID=" + ProductID
+				+ ", Purchase=" + Purchase + ", UnitPrice=" + UnitPrice + "]";
+	}
+	
+	
+	
+	
+	
 }
